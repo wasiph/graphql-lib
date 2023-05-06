ea4843866c8ac3e4b621ba5f4cd5d570b88ffaab
Syed Wasif <wasiph90@gmail.com>
2023-05-06 19:22:22 +0500

    :package: boxing configurations

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
+import config from './common/configs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
